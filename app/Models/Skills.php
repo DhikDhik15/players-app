3d3937be367550a3e2e183d2cdefb8cb54729ee0
Dhika <putraadhika15@gmail.com>
2024-06-30 17:48:23 +0700

    add models

--- app/Models/Skills.php
+++ app/Models/Skills.php
@@ -2,23 +2,23 @@
 
 namespace App\Models;
 
-use App\Models\Skills;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class User extends Model
+class Skills extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $connection = 'mysql';
 
-    protected $table = 'users';
+    protected $table = 'skills';
 
     protected $guarded = ['id'];
 
-    public function skills()
+    public function user()
     {
-        return $this->hasMany(Skills::class, 'user_id');
+        return $this->belongsTo(User::class, 'id', 'user_id');
     }
 }
